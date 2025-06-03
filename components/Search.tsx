8fb0995c8ff6242888606ab43000f74bcebb949b
sindremag <sindre.magnussen@icloud.com>
2025-06-03 12:09:01 +0200

    search button
    endret farge på search button så den er lik logo

--- components/Search.tsx
+++ components/Search.tsx
@@ -30,7 +30,7 @@ const Search = () => {
       {/* Søkeknapp med ikon */}
       <button
         type="submit"
-        className="flex items-center px-4 py-2 bg-green-500 text-white rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="flex items-center px-4 py-2 bg-green-900 text-white rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
       >
         <IconSearch className="mr-2" size={18} />
         Search
