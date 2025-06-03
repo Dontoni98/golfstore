d8f5d96fea45ad6ddd6668ea7ccef03d44e929e4
sindremag <sindre.magnussen@icloud.com>
2025-06-03 11:06:43 +0200

    Endret title
    Tittel endret til "Køllegutta"

--- app/(Auth)/sign-in/layout.tsx
+++ app/(Auth)/sign-in/layout.tsx
@@ -1,14 +1,14 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import { KeycloakProvider } from '../sign-in/provider/KeycloakProvider'; // Juster path
-import type React from 'react';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { KeycloakProvider } from "../sign-in/provider/KeycloakProvider"; // Juster path
+import type React from "react";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "NavBar with Sticky Search",
-  description: "A demo of a navigation bar with a sticky search component",
-}
+  title: "Køllegutta",
+  description: "work in progress",
+};
 
 export default function AuthLayout({
   children,
--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,31 +1,26 @@
-import Footer from "@/components/Footer"
-import Navbar from "@/components/Navbar"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import type React from "react" // Added import for React
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import type React from "react"; // Added import for React
 
-
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "NavBar with Sticky Search",
-  description: "A demo of a navigation bar with a sticky search component",
-}
+  title: "Køllegutta",
+  description: "work in progress",
+};
 
 export default function AuthLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <main>
-        <Navbar />
-        <div className="mt-20">
-        {children}
-        </div>
-        <Footer />
-        
+      <Navbar />
+      <div className="mt-20">{children}</div>
+      <Footer />
     </main>
-   
-  )
+  );
 }
